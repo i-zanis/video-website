17f6945bf3c5ea16b2ed58e1c82c42013568bdb7
i-zanis <ioanlafazanis@gmail.com>
2021-10-02 04:08:30 +0100

    chore(utils): add colorLog()

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+function colorLog(message, color) {
+  color = color || "black";
+  switch (color) {
+    case "green":
+      color = "Green";
+      break;
+    case "blue":
+      color = "DodgerBlue";
+      break;
+    case "red":
+      color = "Red";
+      break;
+    case "orange":
+      color = "Orange";
+      break;
+  }
+  console.log("%c" + message, "color:" + color);
+}
+module.exports = colorLog;
+
