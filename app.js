15c2d986ceb18c2644394928170ba51e1456fd01
i-zanis <ioanlafazanis@gmail.com>
2021-10-17 08:35:15 +0100

    chore(app): add app.listen, morgan, chalk

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const colorLog = require("@/utils");
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({ path: './config.env' });
 const app = express();
-const PORT = 4000;
-app.listen(PORT, () =>{colorLog(`API running on P${PORT}...`),'green'});
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+const PORT = process.env.PORT || 4000;
+const print = console.log;
+app.use((req,
+  res,
+  next) => {
+  res.json({ title: 'test output' });
+});
+
+app.listen(PORT, () => {
+  print(chalk.green(`[${new Date().toLocaleTimeString()}] App running on http://localhost:${PORT}`));
+});
