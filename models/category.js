f4aa2a22ab1a10f294a7fc30bf2dcbe11c6c10e1
i-zanis <ioanlafazanis@gmail.com>
2022-01-24 21:29:35 +0000

    feat(models): category

---
+++ models/category.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+// Adds pre-save validation for unique fields within a Mongoose schema
+// Get violation error on unique constraint violation instead of E1100 from
+// MongoDB
+const uniqueValidator = require('mongoose-unique-validator');
+const { Schema } = require('mongoose');
+
+const categorySchema = new Schema({
+  title: {
+    String,
+    minLength: [3, 'Title requires at least 3 characters'],
+    trim: true,
+    unique: true,
+    // uniqueValidator, won't allow upper/lower combination
+    uniqueCaseInsensitive: true,
+    required: [true, 'Title is required'],
+  },
+  description: {
+    String,
+    minLength: [3, 'Description requires at least 3 characters'],
+    required: [true, 'Description is required'],
+  },
+},
+{ timestamps: true });
+
+categorySchema.plugins(uniqueValidator, { message: '{PATH} already exists' });
+
+module.exports = mongoose.model('Category', categorySchema);
