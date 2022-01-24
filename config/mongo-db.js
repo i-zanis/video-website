8227ce7b8dd1b78fafb8b7abca29bbd4be9b6fbb
i-zanis <ioanlafazanis@gmail.com>
2022-01-24 21:27:16 +0000

    refactor(mongo): move to config folder

---
+++ config/mongo-db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/.env' });
+const print = console.log;
+
+const DBConnect = async () => {
+  const con = await mongoose
+    .connect(process.env.MONGO)
+    .catch((err) => {
+      print(colors.red('Cannot connect to DB'), err);
+    });
+
+  print(colors.bgGreen(`MongoDB connected: ${con.connection.host}`));
+};
+
+module.exports = DBConnect;
