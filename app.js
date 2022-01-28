d35fbc9721318a703c8658fc3650a62e0adc9011
i-zanis <ioanlafazanis@gmail.com>
2022-01-28 00:37:23 +0000

    refactor(app): add json parsing, categoryRoutes

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const DBConnect = require('./config/mongo-db');
+const categoryRoutes = require('./routes/category');
 
 dotenv.config({ path: './config/.env' });
 const app = express();
 const print = console.log;
-DBConnect();
+const PORT = 3000;
 
+DBConnect();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-const PORT = 3000;
-app.use((req,
-  res,
-  next) => {
-  res.json({ title: 'test output' });
-});
+// parses json
+app.use(express.json());
+app.use('/api/v1/categories', categoryRoutes);
 
 app.listen(PORT, () => {
-  print(colors.green(`[${new Date().toLocaleTimeString()}] App running on http://localhost:${PORT}`));
+  print(colors.cyan(`[${new Date().toLocaleTimeString()}] App running on http://localhost:${PORT}`));
 });
