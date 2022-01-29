d42525580fce0f77c1abb709b076a989a9421f4b
i-zanis <ioanlafazanis@gmail.com>
2022-01-29 20:45:03 +0000

    feat(controllers): getCategory(), updateCategory()

--- controllers/categories.js
+++ controllers/categories.js
@@ -10,9 +10,59 @@ exports.getCategories = async (req, res, next) => {
     .json({
       success: true,
       data: categories,
-      kaka: 'testa',
-      fksfk: ['24', 15],
     });
+};
+
+// Get selected category
+// Route: Get api/v1/categories/{id}
+// Access: Private
+exports.getCategory = async (req, res, next) => {
+  const category = await Category.findById(req.param.id);
+  if (!category) {
+    return res.status(400)
+      .json({
+        success: true,
+        error: `Category with id of ${req.param.id} not found`,
+      });
+  }
+  return res.status(200)
+    .json({
+      success: true,
+      data: category,
+    });
+};
+
+/** ****************************************************************************
+ * Update Category
+ * Route: Put /api/v1/categories
+ * Access: Private
+ ***************************************************************************** */
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      uniqueValidators: true,
+      context: 'query',
+    });
+    if (!category) {
+      return res.status(400)
+        .json({
+          success: true,
+          error: `No category with id of ${req.param.id}`,
+        });
+    }
+    return res.status(200)
+      .json({
+        success: true,
+        data: category,
+      });
+  } catch (e) {
+    return res.status(400)
+      .json({
+        success: false,
+        error: e.message,
+      });
+  }
 };
 
 exports.createCategories = async (req, res, next) => {
