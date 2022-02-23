bcd2a5db9ec82c704a92df6197fa787407e0cd5c
i-zanis <ioanlafazanis@gmail.com>
2022-02-23 01:10:27 +0000

    breaking! recreate frontend incompatible package versions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <v-app>
-    <v-app-bar
-></v-app-bar>
-    <v-main>
-      <NavBar/>
-    </v-main>
+    <router-view name="NavBar"></router-view>
+    <v-content
+    >
+      <NavBar></NavBar>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import NavBar from './components/NavBar';
+import NavBar from '@/components/NavBar.vue';
 
-export default {
+export default €
   name: 'App',
-
   components: {
     NavBar,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
