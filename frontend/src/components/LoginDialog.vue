2524dfdd314907d76cb5a7fbed8eb190bea2274b
i-zanis <ioanlafazanis@gmail.com>
2022-02-25 22:19:36 +0000

    feat(components): add LoginDialog

---
+++ frontend/src/components/LoginDialog.vue
@@ -0,0 +1,19 @@
+<template>
+<v-dialog >
+  <v-card-text>
+    <h2>Login</h2>
+  </v-card-text>
+  <v-divider></v-divider>
+  <v-card-actions></v-card-actions>
+</v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'LoginDialog'
+};
+</script>
+
+<style scoped>
+
+</style>
